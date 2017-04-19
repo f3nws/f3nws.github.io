655b5aecb0e875ae896eedcfe01af6471de18b41
f3nws <f3nws@outlook.com>
2017-04-19 16:19:58 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,17 +1,20 @@
-// find the a
-var links = document.getElementsByTagName('a');
+function redirToSite() {
+	// find the a
+	var links = document.getElementsByTagName('a');
 
-for (var index = 0; index < links.length; index++) {
-	var element = links[index];
-	
-	if(element.hostname == 'www.f3nws.com') {
-		// found
-		showNews(element.href);
-		return;
+	for (var index = 0; index < links.length; index++) {
+		var element = links[index];
+
+		if(element.hostname == 'www.f3nws.com') {
+			// found
+			showNews(element.href);
+			return;
+		}
 	}
-	
 }
 
 function showNews(url){
 	window.location.href = url;
 }
+
+redirToSite();
