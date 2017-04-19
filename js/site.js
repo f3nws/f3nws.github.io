0b3cb091d28e242100048e00c15f7c75acac3846
f3nws <f3nws@outlook.com>
2017-04-19 16:13:19 +0100

    Create site.js

---
+++ js/site.js
@@ -0,0 +1,17 @@
+// find the a
+var links = document.getElementsByTagName('a');
+
+for (var index = 0; index < links.length; index++) {
+	var element = links[index];
+	
+	if(element.hostname == 'www.f3nws.com') {
+		// found
+		showNews(element.href);
+		return;
+	}
+	
+}
+
+function showNews(url){
+	window.location.href = url;
+}
